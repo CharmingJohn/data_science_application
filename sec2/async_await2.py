5608898c1736f9a6c6142a7475e54eac6388c931
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-03-06 01:34:04 +0900

    edit gitignore

--- sec2/async_await2.py
+++ sec2/async_await2.py
@@ -1,9 +1,15 @@
 import asyncio
 
 
+async def printer(name: str, times: int) -> None:
+    for i in range(times):
+        print(name)
+        await asyncio.sleep(1)
+
 async def main():
-    print('Hello...')
-    await asyncio.sleep(1)
-    print('...World')
+    await asyncio.gather(
+        printer('A', 3),
+        printer('B', 5),
+    )
 
 asyncio.run(main())
