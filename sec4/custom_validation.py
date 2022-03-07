a295229a65219faafcb93882e914ae23bfe8f71e
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-03-08 08:00:53 +0900

    custom_validation

--- sec4/custom_validation.py
+++ sec4/custom_validation.py
@@ -1,5 +1,5 @@
 from datetime import date
-
+from typing import List
 from pydantic import BaseModel, validator, EmailStr, ValidationError, root_validator
 
 class Person(BaseModel):
@@ -28,3 +28,14 @@ class UserRegistration(BaseModel):
             raise ValueError('Passwords don\'t match')
 
         return values
+
+class Model(BaseModel):
+    values: List[int]
+    @validator('values', pre=True)
+    def split_string_values(cls, v):
+        if isinstance(v, str):
+            return v.split(',')
+        return v
+
+m = Model(values='1,2,3')
+print(m.values)
