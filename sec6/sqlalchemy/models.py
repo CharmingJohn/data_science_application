11ad78f343ac28769b7122280171875819d1d0f3
CharmingJohn <chaechunjae@gmail.com>
2022-04-17 21:40:13 +0900

    edit sec6

--- sec6/sqlalchemy/models.py
+++ sec6/sqlalchemy/models.py
@@ -1,6 +1,25 @@
+from datetime import datetime
+from typing import Optional
+
 import sqlalchemy
+from pydantic import BaseModel, Field
 
 metadata = sqlalchemy.MedtaData()
+
+class PostBase(BaseModel):
+    title: str
+    contetn: str
+    publication_date: datetime = Field(default_factory=datetime.now)
+
+class PostPartialUpdate(BaseModel):
+    title: Optional[str] = None
+    contetn: Optional[str] = None
+
+class PostCreate(PostBase):
+    pass
+
+class PostDB(PostBase):
+    id: int
 
 posts = sqlalchemy.Table(
     "posts",
