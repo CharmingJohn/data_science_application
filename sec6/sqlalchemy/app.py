818f52401c134ca0db431be3252543744ff4b515
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-04-20 03:45:39 +0900

    ch6 more

--- sec6/sqlalchemy/app.py
+++ sec6/sqlalchemy/app.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 from databases import Database
 
 from sec6.sqlalchemy.database import get_database, sqlalchemy_engine
-from sec6.sqlalchemy.models import (metadata, posts, PostDB, PostCreate, PostPartialUpdate)
+from sec6.sqlalchemy.models import (metadata, posts, PostDB, PostCreate, PostPublic, PostPartialUpdate, comments, CommentCreate, CommentDB)
 
 app = FastAPI()
 
@@ -42,7 +42,7 @@ async def list_posts(
 ) -> List[PostDB]:
     skip, limit = pagination
     select_query = posts.select().offset(skip).limit(limit)
-    rows = await databse.fetch_all(select_qeury)
+    rows = await database.fetch_all(select_query)
 
     results = [PostDB(**rows) for row in rows]
     
@@ -64,4 +64,36 @@ async def update_post(
     update_query = (
         posts.update().where(posts.c.id == post.id).values(post_update.dicts(exclude_unset=True))
     )
-    post_id = await databse.execute(update_query)
+    post_id = await database.execute(update_query)
+
+@app.post("/comments", response_model=CommentDB, status_code=status.HTTP_201_CREATED)
+async def create_comment(comment: CommentCreate, database: Database = Depends(get_database)) -> CommentDB:
+
+    select_post_query = posts.select().where(posts.c.id == comment.post_id)
+    post = await database.fetch_one(select_post_query)
+
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"post {id} does not exist")
+
+    insert_query = comments.insert().values(comment.dict())
+    comment_id = await database.execute(insert_query)
+
+    select_query = comments.select().where(comments.c.id == comment_id)
+    raw_comment = cast(Mapping, await database.fetch_one(select_query))
+
+    return CommentDB(**raw_comment)
+
+async def get_post_or_404(
+    id: int, database: Database = Depends(get_database)
+) -> PostPublic:
+    select_post_query = posts.select().where(posts.c.id == id)
+    raw_post = await database.fetch_one(select_post_query)
+
+    if raw_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    select_post_comments_query = comments.select().where(comments.c.post_id == id)
+    raw_comments = await database.fetch_all(select_post_comments_query)
+    comments_list = [CommentDB(**comment) for comment in raw_comments]
+
+    return PostPublic(**raw_post, comments=comments_list)
--- sec6/sqlalchemy/models.py
+++ sec6/sqlalchemy/models.py
@@ -1,10 +1,22 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
+from xml.etree.ElementTree import Comment
 
 import sqlalchemy
 from pydantic import BaseModel, Field
 
 metadata = sqlalchemy.MedtaData()
+
+class CommentBase(BaseModel):
+    post_id: int
+    publication_date: datetime = Field(default_factory=datetime.now)
+    content: str
+
+class CommentCreate(CommentBase):
+    pass
+
+class CommentDB(CommentBase):
+    id: int
 
 class PostBase(BaseModel):
     title: str
@@ -21,6 +33,9 @@ class PostCreate(PostBase):
 class PostDB(PostBase):
     id: int
 
+class PostPublic(PostDB):
+    comments: List[CommentDB]
+
 posts = sqlalchemy.Table(
     "posts",
     metadata,
@@ -28,4 +43,13 @@ posts = sqlalchemy.Table(
     sqlalchemy.Column("publictaion_date", sqlalchemy.DateTime(), nullable=False),
     sqlalchemy.Column("title", sqlalchemy.String(length=255), nullable=False),
     sqlalchemy.Column("content", sqlalchemy.Text(), nullable=False)
+)
+
+comments = sqlalchemy.Table(
+    "comments",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("post_id", ondelete="CASCADE"), nullable=False),
+    sqlalchemy.Column("publication_date", sqlalchemy.DateTime(), nullable=False),
+    sqlalchemy.Column("content", sqlalchemy.Text(), nullable=False),
 )
